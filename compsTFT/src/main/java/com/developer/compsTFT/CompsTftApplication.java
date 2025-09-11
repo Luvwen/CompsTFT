00c4ac4d25585d6e6322516379d19022f3ae3ae7
Santiago <santiagobalino@hotmail.com>
2025-09-11 03:03:58 -0300

    Feat: setup project

---
+++ compsTFT/src/main/java/com/developer/compsTFT/CompsTftApplication.java
@@ -0,0 +1,13 @@
+package com.developer.compsTFT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompsTftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompsTftApplication.class, args);
+	}
+
+}
---
+++ compsTFT/src/test/java/com/developer/compsTFT/CompsTftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.developer.compsTFT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompsTftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
